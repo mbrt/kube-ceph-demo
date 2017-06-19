3fde7e44ed98577b3bb647acf9e1eae82b54e60e
Michele Bertasi <michele.bertasi@brightcomputing.com>
2017-06-19 16:03:09 +0200

    add node on create-pool script

--- create-pool.sh
+++ create-pool.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# refer to the Ceph documentation for the pool parameters
+# the ones provided here work in a fairly small demo cluster
 ceph osd pool create kube 100 100
-ceph osd pool set kube size 3
+ceph osd pool set kube size 2
 ceph osd pool set kube min_size 1
